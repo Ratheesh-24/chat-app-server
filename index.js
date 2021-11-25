146531b93ee9e2f3c6983a66489f969392b55ddf
Ratheesh K <ratheeshkarnan1999@gmail.com>
2021-11-25 21:23:56 +0530

    uploaded

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const PORT=process.env.PORT || 3001;
+
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+app.use(express.static(__dirname + '/../../build'))
+
+app.get('/', (req,res) => {
+  res.send('Chat app Server is up and running')
+})
+
+server.listen(PORT, () => {
+  console.log(`SERVER RUNNING at ${PORT}`);
+});
